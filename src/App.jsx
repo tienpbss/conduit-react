1389ba492ccfd986bb18728c5b879ebab5b1a2fd
tienpbss <tienphung.cn07@gmail.com>
2023-10-20 18:45:40 +0700

    struct project

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import {
+  Home,
+  Login,
+  Navbar,
+  Register,
+  Article,
+  Editor,
+  Profile,
+  Settings,
+  Footer,
+} from "./components";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/editor" element={<Editor />} />
+        <Route path="/editor/:slug" element={<Editor />} />
+        <Route path="/arfile/:username" element={<Profile />} />
+        <Route path="/proticle/:slug" element={<Article />} />
+        <Route path="/profile/:username/favorites" element={<Profile />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { default as Article } from "./article/Article";
+export { default as Editor } from "./editor-article/Editor";
+export { default as Home } from "./home/Home";
+export { default as Login } from "./login/Login";
+export { default as Profile } from "./profile/Profile";
+export { default as Register } from "./register/Register";
+export { default as Settings } from "./settings/Settings";
+
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
